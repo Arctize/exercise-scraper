ac015c03cb10ff48eac9e0f0e89c3f4757a8f1b2
arctize <patrick.wicki96@gmail.com>
2018-03-16 06:37:40 +0100

    Added pprog

--- scraper.py
+++ scraper.py
@@ -32,7 +32,7 @@ dirs = ["exercises/analysis", "exercises/dc",
         "exercises/pprog", "exercises/algowar"]
 
 for i in dirs:
-    if not os.path.isdir(i):
+   if not os.path.isdir(i):
         print("Creating directory: ", i)
         os.makedirs(i)
 print()
@@ -97,7 +97,7 @@ def ana():
                         sys.stdout.write(
                             "\r -> Downloading %-20s [%s]" % (filename, progress))
                         sys.stdout.flush()
-                    print()
+                print()
     print('\n')
 
 
@@ -157,10 +157,63 @@ def aw():
     print('\n')
 
 
-def dc():  # Website is not https -> Not implementing this yet.
-    print('[3/4] Design of Digital Circuits')
+def pp():
+    print('[3/4] Parallel Programming')
+    url='https://www.srl.inf.ethz.ch/'
 
-    url = 'http://www.srl.inf.ethz.ch/pp2018.php'
+    request = Request(url + 'pp2018.php', headers=headers)
+    try:
+        soup = BeautifulSoup(urllib.request.urlopen(request), 'html.parser')
+    except urllib.error.URLError:
+        input("No internet connection - connect to internet and try again.")
+        sys.exit(0)
+    print('\n')
+
+    lpart = 'Presentation Schedule'
+    rpart = 'Exams and Grading'
+    soup = str(soup).partition(lpart)[-1]
+    soup = soup.rpartition(rpart)[0]
+    soup = BeautifulSoup(soup, 'html.parser')
+
+    links = []
+    for link in soup.find_all('a'):
+        link = link['href']
+        links.append(link)
+
+    getLoginInfo()
+    basedir = dirs[2]
+    for link in links:
+        path = basedir + '/' + link.split('/', 2)[-1]
+        filename = os.path.basename(path)
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+        if not os.path.isfile(path):
+            with open(path, 'wb') as f:
+                response = requests.get(
+                    url + link, stream=True, headers=headers, auth=(login['name'], login['password']))
+                total_length = response.headers.get('content-length')
+                if total_length is None:  # no content length header
+                    f.write(response.content)
+                    sys.stdout.write("\r -> Downloading: %-20s" % (filename))
+                    sys.stdout.flush()
+                else:  # fancy progress bar, because why not
+                    dl = 0
+                    total_length = int(total_length)
+                    for data in response.iter_content(chunk_size=4096):
+                        dl += len(data)
+                        f.write(data)
+                        width = 40
+                        done = int(width * dl / total_length)
+                        progress = '=' * done + '>' + ' ' * (width - done)
+                        sys.stdout.write(
+                            "\r -> Downloading %-20s [%s]" % (filename, progress))
+                        sys.stdout.flush()
+                print()
+    print('\n')
+
+def dc():
+    print('[4/4] Design of Digital Circuits')
+    url = 'https://safari.ethz.ch/digitaltechnik/spring2018/doku.php?id=labs'
     basedir = dirs[1]
 
     request = Request(url, headers=headers)
@@ -169,11 +222,15 @@ def dc():  # Website is not https -> Not implementing this yet.
     except urllib.error.URLError:
         input("No internet connection - connect to internet and try again.")
         sys.exit(0)
+        
+    # TODO
+
     print('\n')
 
 
 ana()
 aw()
+pp()
 # dc()
 
 
